e2fff55068d71aa366f662afc282220f3d8bfbee
Roland Häder <roland@mxchange.org>
2017-01-27 11:53:56 +0100

    added spaces (coding convention)
    Signed-off-by: Roland Häder <roland@mxchange.org>

--- view/theme/frio/php/PHPColors/Color.php
+++ view/theme/frio/php/PHPColors/Color.php
@@ -36,7 +36,7 @@ class Color {
         // Make sure it's 6 digits
         if ( strlen($color) === 3 ) {
             $color = $color[0].$color[0].$color[1].$color[1].$color[2].$color[2];
-        } elseif ( strlen($color) != 6 ) {
+        } else if ( strlen($color) != 6 ) {
             throw new Exception("HEX color needs to be 6 or 3 digits long");
         }
 
@@ -484,7 +484,7 @@ class Color {
         // Make sure it's 6 digits
         if ( strlen($color) == 3 ) {
             $color = $color[0].$color[0].$color[1].$color[1].$color[2].$color[2];
-        } elseif ( strlen($color) != 6 ) {
+        } else if ( strlen($color) != 6 ) {
             throw new Exception("HEX color needs to be 6 or 3 digits long");
         }
 
